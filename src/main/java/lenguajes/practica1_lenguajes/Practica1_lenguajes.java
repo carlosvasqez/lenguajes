b78eb3eb059b0348efe4b107d1c7a641dbfd4d6b
carlos vasquez <carlosvasquez201630832@cunoc.edu.gt>
2023-08-14 03:24:13 -0600

    proyecto maven inicializado

---
+++ src/main/java/lenguajes/practica1_lenguajes/Practica1_lenguajes.java
@@ -0,0 +1,14 @@
+
+
+package lenguajes.practica1_lenguajes;
+
+/**
+ *
+ * @author usuario
+ */
+public class Practica1_lenguajes {
+
+    public static void main(String[] args) {
+        System.out.println("Hello World!");
+    }
+}
